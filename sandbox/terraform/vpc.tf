94c0292fcd44a347208c47a9f165ea3a48daae19
knysh90 <knysh.romank@gmail.com>
2024-07-10 20:01:57 -0400

    update version

--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "my-vpc-AFT-TEST-${data.aws_caller_identity.current.account_id}"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
